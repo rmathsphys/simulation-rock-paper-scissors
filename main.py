d553e68c036f5bbe241735e1f5daaaa5ac0e3026
rmathsphys <61981812+rmathsphys@users.noreply.github.com>
2023-03-12 12:22:52 +0500

    Exposed more functionality to the user: setting frames and strenghts.

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from rpsgame import RPSGame
 
 model = RPSGame(n_rock=5, n_paper=5, n_scissors=5, width=1)
 model.play()
+model.set_frames(100)
+model.set_hf(0.005)
+model.set_ha(0.02)
+model.set_ht(0.03)
 model.play()
 # model.export('new.mp4')
--- rpsgame.py
+++ rpsgame.py
@@ -20,8 +20,21 @@ class RPSGame():
         self.fr = '#002e63'
         self.fp = '#ffa700'
         self.fs = '#ae0c00'
+        self.frame = 1000
 
         self._re_init()
+
+    def set_hf(self, val):
+        self.hf = val
+
+    def set_ha(self, val):
+        self.ha = val
+
+    def set_ht(self, val):
+        self.ht = val
+
+    def set_frames(self, val):
+        self.frames = val
 
     def _re_init(self):
         while True:
@@ -37,9 +50,7 @@ class RPSGame():
     def _warm_up_check(self):
         ''' Initial repelling and error handling to make sure
         that no two elements are 'too' close to each other '''
-        self._dm = np.array(
-            [np.sum((self.pos - self.pos[j])**2, axis=-1) for j in range(self.nt)]
-            )
+        self._dm = np.array([np.sum((self.pos - self.pos[j])**2, axis=-1) for j in range(self.nt)])
         return np.any(np.less(self._dm[self._dm>0], 1.2*self.threshold))
 
     def _step_sim(self):
@@ -52,7 +63,7 @@ class RPSGame():
                 dist_f = np.sum((arr_f - self.pos[j])**2, axis=-1)
                 dist_f = np.where(dist_f == 0, np.max(dist_f)+1, dist_f)
                 rf = arr_f[np.argmin(dist_f)] - self.pos[j]
-                rf = rf / max(1e-8, np.sum(rf**2))
+                rf = rf / max(1e-12, np.sum(rf**2))
             else:
                 rf = 0
             
@@ -60,7 +71,7 @@ class RPSGame():
                 arr_a = self.pos[self.cat == ((self.cat[j]+1)%3)]
                 dist_a = np.sum((arr_a - self.pos[j])**2, axis=-1)
                 ra = arr_a[np.argmin(dist_a)] - self.pos[j]
-                ra = ra / max(1e-8, np.sum(ra**2))
+                ra = ra / max(1e-12, np.sum(ra**2))
             else:
                 ra = 0
             
@@ -68,21 +79,18 @@ class RPSGame():
                 arr_t = self.pos[self.cat == ((self.cat[j]+2)%3)]
                 dist_t = np.sum((arr_t - self.pos[j])**2, axis=-1)
                 rt = arr_t[np.argmin(dist_t)] - self.pos[j]
-                rt = rt / max(1e-8, np.sum(rt**2))
+                rt = rt / max(1e-12, np.sum(rt**2))
             else:
                 rt = 0
 
             r = (self.ht * rt) - (self.ha * ra) - (self.hf * rf)
             self._temp_pos[j] = self.pos[j] + r
 
-            if self._temp_pos[j,0] > self.w: self._temp_pos[j,0] = (self.w + 0.5*self.threshold)
-            if self._temp_pos[j,0] < -self.w: self._temp_pos[j,0] = -(self.w + 0.5*self.threshold)
-            if self._temp_pos[j,1] > self.w: self._temp_pos[j,1] = (self.w + 0.5*self.threshold)
-            if self._temp_pos[j,1] < -self.w: self._temp_pos[j,1] = -(self.w + 0.5*self.threshold)
+            if self._temp_pos[j,0] > self.w: self._temp_pos[j,0] = (self.w + self.threshold)
+            if self._temp_pos[j,0] < -self.w: self._temp_pos[j,0] = -(self.w + self.threshold)
+            if self._temp_pos[j,1] > self.w: self._temp_pos[j,1] = (self.w + self.threshold)
+            if self._temp_pos[j,1] < -self.w: self._temp_pos[j,1] = -(self.w + self.threshold)
 
-            # CAN GO OUT OF THE SCREEN. NEEDS TO BE FIXED!
-
-        
         for j in range(self.nt):
             if np.any(self.cat == ((self.cat[j]+1)%3)):
                 arr_a = self.pos[self.cat == ((self.cat[j]+1)%3)]
@@ -95,23 +103,6 @@ class RPSGame():
                 self._temp_cat[j] = self.cat[j]
 
         return self._temp_pos, self._temp_cat
-
-    def _display_scene(self, pos_dt, cat_dt):
-        wq = self.fq * self.w
-        ms = 50
-
-        _, ax = plt.subplots(figsize=(4.5, 4.5), facecolor=self.fc, num='The Arena')
-        ax.set(aspect=True, xlim=(-wq, wq), ylim=(-wq, wq))
-        ax.axis(False)
-
-        _ = ax.scatter(pos_dt[cat_dt == 0][:,0], pos_dt[cat_dt == 0][:,1],
-            s=ms, marker=r'$R$', c=self.fr)
-        _ = ax.scatter(pos_dt[cat_dt == 1][:,0], pos_dt[cat_dt == 1][:,1],
-            s=ms, marker=r'$P$', c=self.fp)
-        _ = ax.scatter(pos_dt[cat_dt == 2][:,0], pos_dt[cat_dt == 2][:,1],
-            s=ms, marker=r'$S$', c=self.fs)
-
-        plt.show()
 
     def _run_sim(self, export=False, fname='output.mp4'):
         wq = self.fq * self.w
@@ -144,11 +135,11 @@ class RPSGame():
             return sc_r, sc_p, sc_s
         
         if export:
-            ani = anim.FuncAnimation(fig, updater, frames=1000, interval=30, repeat=False, blit=True)
+            ani = anim.FuncAnimation(fig, updater, frames=self.frames, interval=30, repeat=False, blit=True)
             writer = anim.FFMpegWriter(fps=35)
             ani.save(f'{fname}', dpi=256, writer=writer, savefig_kwargs={'facecolor':self.fc})
         else:
-            ani = anim.FuncAnimation(fig, updater, frames=1000, interval=30, repeat=False, blit=True)
+            ani = anim.FuncAnimation(fig, updater, frames=self.frames, interval=30, repeat=False, blit=True)
 
         plt.show()
         self._re_init()
